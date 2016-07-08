57e57f59da25702db025015695e6d01ddb7e5792
Ayase-252 <bitdqy@hotmail.com>
2016-07-08 14:28:06 +0800

    add functions to read info from file

--- bangumi.py
+++ bangumi.py
@@ -33,6 +33,24 @@ def parse_from_json(json_str):
     return new_bangumi
 
 
+def parsed_json_to_dict(parsed):
+    new_bangumi = {}
+    new_bangumi['name'] = parsed['name']
+    new_bangumi['start_date'] = datetime.strptime(
+        parsed['start_date'], '%Y-%m-%d').date()
+    if 'translation_team' in parsed:
+        new_bangumi['translation_team'] = parsed['translation_team']
+    else:
+        new_bangumi['translation_team'] = []
+    if 'total_ep' in parsed:
+        new_bangumi['total_ep'] = int(parsed['total_ep'])
+    else:
+        new_bangumi['total_ep'] = 99
+    new_bangumi['downloaded_ep'] = 0
+    new_bangumi['offset'] = 0
+    return new_bangumi
+
+
 def read_bangumi_from_file(filepath):
     """
     Read bangumi infomation from file, file should be writen in JSON format
@@ -40,24 +58,11 @@ def read_bangumi_from_file(filepath):
     param:
     filepath        path of file providing bangumi info
     """
-    f = open(filepath, 'r')
+    f = open(filepath, 'r', encoding='utf-8')
     parsed = json.loads(f.read(), encoding='utf-8')
     f.close()
     bangumi_list = []
     for bangumi in parsed:
-        new_bangumi = {}
-        new_bangumi['name'] = bangumi['name'],
-        new_bangumi['start_date'] = datetime.strptime('%Y-%m-%d',
-                                                      bangumi['start_date']).date()
-        if bangumi['translation_team'] is not None:
-            new_bangumi['translation_team'] = bangumi['translation_team']
-        else:
-            new_bangumi['translation_team'] = []
-        if bangumi['total_ep'] is not None:
-            new_bangumi['total_ep'] = int(bangumi['total_ep'])
-        else:
-            new_bangumi['total_ep'] = 99
-        new_bangumi['downloaded_ep'] = 0
-        new_bangumi['offset'] = 0
+        new_bangumi = parsed_json_to_dict(bangumi)
         bangumi_list.append(new_bangumi)
     return bangumi_list
--- test_bangumi.py
+++ test_bangumi.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from datetime import date
 
 import bangumi
@@ -6,11 +7,13 @@ import bangumi
 
 class BangumiTestClass(unittest.TestCase):
 
-    def test_parse_from_json_ascii(self):
+    def test_parsed_json_to_dict_ascii(self):
         f = open(r'test-input-ascii.json', 'r', encoding='utf-8')
         test_str = f.read()
-        parsed = bangumi.parse_from_json(test_str)
-        self.assertEqual(parsed, {
+        f.close()
+        parsed = json.loads(test_str, encoding='utf-8')
+        pdict = bangumi.parsed_json_to_dict(parsed)
+        self.assertEqual(pdict, {
             'name': 'Ano hi mita hana no namae wo bokutachi wa mada shiranai.',
             'start_date': date(2011, 4, 14),
             'translation_team': ['sumisora', 'emd'],
@@ -18,13 +21,14 @@ class BangumiTestClass(unittest.TestCase):
             'downloaded_ep': 0,
             'offset': 0
         })
-        f.close()
 
-    def test_parse_from_json_ch(self):
+    def test_parsed_json_to_dict_ch(self):
         f = open(r'test-input-ch.json', 'r', encoding='utf-8')
         test_str = f.read()
-        parsed = bangumi.parse_from_json(test_str)
-        self.assertEqual(parsed, {
+        f.close()
+        parsed = json.loads(test_str, encoding='utf-8')
+        pdict = bangumi.parsed_json_to_dict(parsed)
+        self.assertEqual(pdict, {
             'name': '少女与战车',
             'start_date': date(2012, 10, 8),
             'translation_team': ['测试字幕组'],
@@ -34,11 +38,13 @@ class BangumiTestClass(unittest.TestCase):
         })
         f.close()
 
-    def test_parse_from_json_jp(self):
+    def test_parsed_json_to_dict_jp(self):
         f = open(r'test-input-jp.json', 'r', encoding='utf-8')
         test_str = f.read()
-        parsed = bangumi.parse_from_json(test_str)
-        self.assertEqual(parsed, {
+        f.close()
+        parsed = json.loads(test_str, encoding='utf-8')
+        pdict = bangumi.parsed_json_to_dict(parsed)
+        self.assertEqual(pdict, {
             'name': 'ふらいんうぃち',
             'start_date': date(2016, 4, 9),
             'translation_team': [],
@@ -47,3 +53,29 @@ class BangumiTestClass(unittest.TestCase):
             'offset': 0
         })
         f.close()
+
+    def test_read_bangumi_from_file(self):
+        read_list = bangumi.read_bangumi_from_file('test-input.json')
+        self.assertEqual(read_list, [{
+            'name': 'Ano hi mita hana no namae wo bokutachi wa mada shiranai.',
+            'start_date': date(2011, 4, 14),
+            'translation_team': ['sumisora', 'emd'],
+            'total_ep': 11,
+            'downloaded_ep': 0,
+            'offset': 0
+        }, {
+            'name': 'ふらいんうぃち',
+            'start_date': date(2016, 4, 9),
+            'translation_team': [],
+            'total_ep': 12,
+            'downloaded_ep': 0,
+            'offset': 0
+        }, {
+            'name': '少女与战车',
+            'start_date': date(2012, 10, 8),
+            'translation_team': [],
+            'total_ep': 99,
+            'downloaded_ep': 0,
+            'offset': 0
+        }
+        ])

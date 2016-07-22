ff598b67637ef6ce083947ef23f5fb8269f89a30
Ayase-252 <bitdqy@hotmail.com>
2016-07-22 11:50:58 +0800

    refactored download with requests

--- downloader.py
+++ downloader.py
@@ -3,8 +3,9 @@ Downloader
 
 This module defines function to download torrent from Internet
 """
-from urllib import request
 import os
+
+import requests
 
 
 def download(url, name, ep, translation_team='', save_path='', **kargs):
@@ -19,9 +20,14 @@ def download(url, name, ep, translation_team='', save_path='', **kargs):
     save_path:          Path where the file will be saved
     """
     print('Downloading {} {} from {}'.format(name, ep, url))
-    response = request.urlopen(url)
+    user_agent = {
+        'User-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML,'
+                      'like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+    }
+    response = requests.get(url, headers=user_agent, timeout=5)
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     f = open(save_path + name + '-ep.' + str(ep) + '-' +
              '.torrent', 'wb')
-    f.write(response.read())
+    f.write(response.content)
+    f.close()

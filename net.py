659539c8e7010cf24de70a1aaf2c7985412bf683
Ayase-252 <bitdqy@hotmail.com>
2016-07-24 17:43:28 +0800

    add request_get_content

--- net.py
+++ net.py
@@ -5,28 +5,70 @@ The module provides methods to access internet resource such as obtaining web
 content and download file. To keep all internet accesses coordinated, all codes
 SHALL make them request via methods in this module
 """
+import os
+import time
 
 import requests
 
 
-def download(uri, file_path):
+def download(url, file_name, save_path=''):
     """
-    Download file from location indicated by uri, and save it in file_path
+    Download file from location indicated by url, and save it in save_path
 
     params:
-    uri         Resource uri
-    file_path   Path of downloaded file
+    url         Resource url
+    file_name   Name of downloaded file
+    save_path   Saving path of downloaded file
     """
-    pass
+    response = _make_get_request(url)
+    if save_path != '' and not os.path.exists(save_path):
+        os.makedirs(save_path)
+    f = open(save_path + file_name, 'wb')
+    f.write(response.content)
+    f.close()
 
 
-def _make_get_request(uri, params=[]):
+def request_get_content(url, retry=0, retry_interval=5):
     """
-    Make HTTP GET requests to assigned uri and parameters
+    Get content of response subject to request to url
 
     params:
-    uri         Resource uri
+    url             Resource url
+    retry           Max retry time
+    retry_interval  Time interval between retries
+    """
+    try_time = retry + 1
+    r = None
+    while r is None or r.status_code != 200:
+        try:
+            r = _make_get_request(url)
+        except Exception as e:
+            print('\n\nException raised\n', e)
+        try_time -= 1
+        if r.status_code != 200:
+            print('Error: Request returns response with status code',
+                  r.status_code)
+        if try_time == 0:
+            break
+        print('Retry will starts in {} second'.format(retry_interval))
+        time.sleep(retry_interval)
+    if r is None or r.status_code != 200:
+        raise RuntimeError(
+            'Reach maximun retry time in requesting url {}'.format(url))
+    else:
+        return r
+
+
+def _make_get_request(url, params=[]):
+    """
+    Make HTTP GET requests to assigned url and parameters
+
+    params:
+    url         Resource url
     params      Additional parameters bore by request
+
+    return:
+    response object returned by requests lib
     """
     default_headers = {
         'User-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML,'
@@ -34,5 +76,5 @@ def _make_get_request(uri, params=[]):
     }
 
     default_time_out = 10
-    return requests.get(uri, params=params,
+    return requests.get(url, params=params,
                         headers=default_headers, timeout=default_headers)
--- test/test_net.py
+++ test/test_net.py
@@ -4,6 +4,7 @@ Tests for Net interface module
 It requires requests-mock
 """
 import unittest
+import os
 
 import requests_mock
 
@@ -15,6 +16,27 @@ requests_mock.Mocker.TEST_PREFIX = 'test_'
 class NetInterfaceTests(unittest.TestCase):
 
     def test_make_get_request(self, mocker):
-        mocker.register_url('GET', 'http://test.com', content='Hello world')
-        r = net._make_get_request('https://test.com')
-        self.assertEqual('Hello world', r.content)
+        mocker.register_uri('GET', 'http://test.com', text='Hello world')
+        r = net._make_get_request('http://test.com')
+        self.assertEqual('Hello world', r.text)
+
+    def test_download(self, mocker):
+        mocker.register_uri('GET', 'http://test.com', content=b'Hello world')
+        net.download('http://test.com', 'test.file')
+        f = open('test.file', 'rb')
+        content = f.read()
+        f.close()
+        os.remove('test.file')
+        self.assertEqual(b'Hello world', content)
+
+    def test_request_get_content(self, mocker):
+        mocker.register_uri('GET', 'http://test.com', content=b'<html></html>')
+        content = net.request_get_content('http://test.com')
+        self.assertEqual(b'<html></html>', content.content)
+
+    def test_request_get_content_retry_fail(self, mocker):
+        mocker.register_uri('GET', 'http://test.com',
+                            text='Not found', status_code=404)
+        self.assertRaises(RuntimeError,
+                          net.request_get_content,
+                          'http://test.com', 2, 0.1)

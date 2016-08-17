6e88c1fec011c9e91604778d08bec2cff4d1172b
Ayase-252 <bitdqy@hotmail.com>
2016-08-17 17:12:04 +0800

    add a plugin named add into management system

--- manage.py
+++ manage.py
@@ -4,10 +4,21 @@ Management System Module
 This module provides user-friendly method to manage database via CLI.
 """
 
+from manage_plugins import Add
 
-def entry(args):
+_registed_plugins = {'add': Add}
+
+
+def entry(*args):
     """
     Entry point of management system. This method handles user input.
     """
     print('Welcome to Management System')
     print('Following commands are available:')
+    for key, value in _registed_plugins.items():
+        print(key + ': ' + value.get_description())
+    print('\nIf you have nothing to do, input exit')
+    user_input = input('')
+    if user_input == 'exit':
+        return
+    _registed_plugins[user_input].func()
